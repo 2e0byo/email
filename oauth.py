135725ddbf5c788b2487effe7b27708a81e3756b
John Maximilian <2e0byo@gmail.com>
2022-05-31 15:10:43 +0100

    feat: oauth.py.

---
+++ oauth.py
@@ -0,0 +1,6 @@
+from email import GmailCredentials, Office365Credentials
+from pathlib import Path
+
+Gmail_2e0byo = GmailCredentials(Path(".pass/2e0byo"))
+Wombat = GmailCredentials(Path(".pass/Wombat"))
+Durham = Office365Credentials(Path(".pass/Durham"))
