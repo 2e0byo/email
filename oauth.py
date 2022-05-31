f6780e36dd507b91f7c70cc1be9f63824e398c7c
John Maximilian <2e0byo@gmail.com>
2022-05-31 16:12:54 +0100

    feat: proper script.

--- oauth.py
+++ oauth.py
@@ -1,11 +1,11 @@
 # path trickery for offlineimap. Other option is to install system-wide...
 import sys
+from argparse import ArgumentParser
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parent))
 
 from email_auth import GmailCredentials, Office365Credentials
-from pathlib import Path
 
 Gmail_2e0byo = GmailCredentials(Path("~/.pass/2e0byo").expanduser())
 Wombat = GmailCredentials(Path("~/.pass/Wombat").expanduser())
@@ -17,10 +17,13 @@ ACCOUNTS = {
     "Durham": Durham,
 }
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        raise Exception(
-            "Provide accounts to update refresh token out of "
-            + " ".join(ACCOUNTS.keys())
-        )
-    for k in sys.argv[1:]:
-        ACCOUNTS[k].write_refresh_token()
+    parser = ArgumentParser()
+    parser.add_argument("account", nargs="*")
+    parser.add_argument("--refresh", help="Refresh token", action="store_true")
+    args = parser.parse_args()
+    for k in args.account:
+        account = ACCOUNTS[k]
+        if args.refresh:
+            account.refresh_token()
+        else:
+            print(account.get_authentication_token())
